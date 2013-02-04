f03be3c3eded03deb9d901e9e52e3b29ba106e92
Max Mustermann <max.mustermann@jobleads.de>
2013-02-04 09:45:25 +0100

    Added force filesystem check.

---
+++ manage/forceFsCheck.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########
+# Force system to check fileystem on next book.
+#
+# @author stev leibelt
+# @since 2013-02-04
+########
+
+sudo touch /forcefsck
+
+echo 'You should reboot now'
